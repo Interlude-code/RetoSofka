9cedbf0cc8b0a75fe3bd62bb784a393bd72c4377
iSebastianCabrera <jscrojas25@yahoo.es>
2021-09-12 13:28:22 -0500

    ultimos retoque a las exportaciones

--- exportarCSV.py
+++ exportarCSV.py
@@ -18,9 +18,19 @@ class exportToCsv():
         for preguntas in results:
             self.bancoPreguntas.append([preguntas["_id"],preguntas["categoria"],preguntas["enunciado"],preguntas["opcion1"],preguntas["opcion2"],preguntas["opcion3"],preguntas["opcion4"],preguntas["correcta"]])
             
-        with open('jugadores.csv','w',newline="") as resultCSV:
+        with open('preguntas.csv','w',newline="") as resultCSV:
             writer = csv.writer(resultCSV,delimiter=",")
             for fila in self.bancoPreguntas:
+                writer.writerow(fila)
+        
+    def exportarJugadoresCsv(self):
+        results=players.find({}) 
+        for jugadores in results:
+            self.bancoJugadores.append([jugadores["_id"],jugadores["nombre"],jugadores["nivelAlcanzado"],jugadores["premioLogrado"]])
+            
+        with open('jugadores.csv','w',newline="") as resultCSV:
+            writer = csv.writer(resultCSV,delimiter=",")
+            for fila in self.bancoJugadores:
                 writer.writerow(fila)
 
     
--- main.py
+++ main.py
@@ -10,25 +10,26 @@ while True:
     opt= input('''Ingrese el numero para la accion seleccionada :
 1. Para iniciar una partida
 2. Ver estadisticas de jugadores
-3. Ingresar nuevas preguntas
-4. Imprimir preguntas
+3. Imprimir preguntas
+4. Ingresar nuevas preguntas 
 5. Para salir
 ''')
     if opt== '1':
         nombre=input("Ingresa el nombre del participante: ")
         nuevoJugador=Jugador(nombre)
         juego.rondas(nuevoJugador)
-
-        
     elif opt== '2':
-        juego.recordJugadores()
-        exportToCsv().reporteJugadores()
+       opt=int(input("Selecciona 1.Para imprimir por consola 2.Para imprimir en .csv :   "))
+       if (opt ==1):
+            juego.recordJugadores() 
+       elif (opt==2):
+            exportToCsv().exportarJugadoresCsv()
     elif opt== '3':
-       opt=int(input("Selecciona 1.Para imprimir por consola 2.Para imprimir en .csv"))
+       opt=int(input("Selecciona 1.Para imprimir por consola 2.Para imprimir en .csv :   "))
        if (opt ==1):
             juego.imprimirPreguntas()    
        elif (opt==2):
-           exportToCsv().exportarPreguntasCsv
+           exportToCsv().exportarPreguntasCsv()
     elif opt== '4':
        juego.ingresarPregunta()
     elif opt== '5':
